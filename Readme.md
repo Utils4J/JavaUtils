ba44174797c3368ee98d8f3c2788bb7d806c4178
MineKing <mineking9534@gmail.com>
2023-12-13 15:09:12 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,38 @@
+![[Java CI]](https://github.com/MineKing9534/JavaUtils/actions/workflows/check.yml/badge.svg)
+![[Latest Version]](https://maven.mineking.dev/api/badge/latest/releases/de/mineking/JavaUtils?prefix=v&name=Latest%20Version)
+
+# Installation
+
+JavaUtils is hosted on a custom repository at [https://maven.mineking.dev](https://maven.mineking.dev/releases/de/mineking/JavaUtils). Replace VERSION with the lastest version (without the `v` prefix).
+Alternatively, you can download the artifacts from jitpack (not recommended).
+
+### Gradle
+
+```groovy
+repositories {
+  maven { url "https://maven.mineking.dev/releases" }
+}
+
+dependencies {
+  implementation "de.mineking:JavaUtils:VERSION"
+}
+```
+
+### Maven
+
+```xml
+<repositories>
+  <repository>
+    <id>mineking</id>
+    <url>https://maven.mineking.dev/releases</url>
+  </repository>
+</repositories>
+
+<dependencies>
+  <dependency>
+    <groupId>de.mineking</groupId>
+    <artifactId>JavaUtils</artifactId>
+    <version>VERSION</version>
+  </dependency>
+</dependencies>
+```
---
+++ src/main/java/de/mineking/javautils/id/ID.java
@@ -0,0 +1,75 @@
+package de.mineking.javautils.id;
+
+import io.seruco.encoding.base62.Base62;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+public class ID {
+	private final static Base62 base62 = Base62.createInstance();
+	public final static int size = Long.BYTES + Byte.BYTES; //Timestamp + Serial
+
+	private static long lastTime = 0;
+	private static byte serial;
+
+	@NotNull
+	private static String toString(@NotNull byte[] bytes) {
+		return new String(base62.encode(bytes), StandardCharsets.UTF_8);
+	}
+
+	@NotNull
+	private static byte[] fromString(@NotNull String str) {
+		return base62.decode(str.getBytes(StandardCharsets.UTF_8));
+	}
+
+	@NotNull
+	public synchronized static ID generate() {
+		var time = System.currentTimeMillis();
+		var buffer = ByteBuffer.allocate(size);
+
+		if(time > lastTime) serial = Byte.MIN_VALUE;
+		else if(serial == Byte.MAX_VALUE) {
+			serial = Byte.MIN_VALUE;
+			time++;
+		}
+
+		buffer.putLong(time);
+		buffer.put(serial++);
+
+		lastTime = time;
+
+		return new ID(toString(buffer.array()));
+	}
+
+	@NotNull
+	public static ID decode(@NotNull String id) {
+		return new ID(id);
+	}
+
+	private final String string;
+
+	ID(String string) {
+		this.string = string;
+	}
+
+	@NotNull
+	public String asString() {
+		return string;
+	}
+
+	@NotNull
+	public BigInteger asNumber() {
+		return new BigInteger(bytes().array());
+	}
+
+	@NotNull
+	public ByteBuffer bytes() {
+		return ByteBuffer.wrap(fromString(string));
+	}
+
+	public long getTimeCreated() {
+		return bytes().getLong();
+	}
+}
