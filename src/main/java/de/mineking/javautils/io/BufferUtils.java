b82ba1187569563e1017dc1a36a170a70f3dd028
cyklon73 <87936307+cyklon73@users.noreply.github.com>
2024-01-20 12:12:10 +0100

    IO (#3)
    * implemented FileUtils
    
    * implemented BufferUtils
    
    * added @NotNull
    
    * Reformat code
    
    ---------
    
    Co-authored-by: MineKing <mineking9534@gmail.com>

---
+++ src/main/java/de/mineking/javautils/io/BufferUtils.java
@@ -0,0 +1,30 @@
+package de.mineking.javautils.io;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+public final class BufferUtils {
+
+	private BufferUtils() {}
+
+	@NotNull
+	public static ByteBuffer wrapFile(@NotNull String name) throws IOException {
+		return ByteBuffer.wrap(FileUtils.readBytes(name));
+	}
+
+	@NotNull
+	public static ByteBuffer wrapFile(@NotNull File file) throws IOException {
+		return ByteBuffer.wrap(FileUtils.readBytes(file));
+	}
+
+	public static void saveBuffer(@NotNull File file, @NotNull ByteBuffer buffer) throws IOException {
+		FileUtils.saveBytes(file, buffer.array());
+	}
+
+	public static void saveBuffer(@NotNull String name, @NotNull ByteBuffer buffer) throws IOException {
+		FileUtils.saveBytes(name, buffer.array());
+	}
+}
---
+++ src/main/java/de/mineking/javautils/io/FileUtils.java
@@ -0,0 +1,48 @@
+package de.mineking.javautils.io;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public final class FileUtils {
+
+	private FileUtils() {}
+
+	public static byte[] readBytes(@NotNull File file) throws IOException {
+		try(FileInputStream fis = new FileInputStream(file)) {
+			return fis.readAllBytes();
+		}
+	}
+
+	public static byte[] readBytes(@NotNull String name) throws IOException {
+		try(FileInputStream fis = new FileInputStream(name)) {
+			return fis.readAllBytes();
+		}
+	}
+
+	public static void saveBytes(@NotNull File file, byte[] data) throws IOException {
+		try(FileOutputStream fos = new FileOutputStream(file)) {
+			fos.write(data);
+		}
+	}
+
+	public static void saveBytes(@NotNull String name, byte[] data) throws IOException {
+		try(FileOutputStream fos = new FileOutputStream(name)) {
+			fos.write(data);
+		}
+	}
+
+	@NotNull
+	public static String getExtension(@NotNull File file) {
+		return getExtension(file.getName());
+	}
+
+	@NotNull
+	public static String getExtension(@NotNull String name) {
+		while(name.endsWith(".")) name = name.substring(0, name.length() - 1);
+		return name.substring(name.lastIndexOf('.') + 1);
+	}
+}
