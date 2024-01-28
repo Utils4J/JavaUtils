db048e0148aeed5b7323fba93c571443308b161b
cyklon73 <cyklon698@gmail.com>
2024-01-28 21:14:10 +0100

    added documentation

--- src/main/java/de/mineking/javautils/function/ThrowingRunnable.java
+++ src/main/java/de/mineking/javautils/function/ThrowingRunnable.java
@@ -3,6 +3,17 @@ package de.mineking.javautils.function;
 @FunctionalInterface
 public interface ThrowingRunnable<E extends Throwable> {
 
+	/**
+	 * When an object implementing interface {@code Runnable} is used
+	 * to create a thread, starting the thread causes the object's
+	 * {@code run} method to be called in that separately executing
+	 * thread.
+	 * <p>
+	 * The general contract of the method {@code run} is that it may
+	 * take any action whatsoever.
+	 *
+	 * @see     java.lang.Thread#run()
+	 */
 	void run() throws E;
 
 }
