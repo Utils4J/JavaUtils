0772e55b804cca622a9172bd2ac71375b75dfdca
cyklon73 <cyklon698@gmail.com>
2024-02-07 16:55:15 +0100

    set methods to public

--- src/main/java/de/mineking/javautils/function/TryCatch.java
+++ src/main/java/de/mineking/javautils/function/TryCatch.java
@@ -15,27 +15,27 @@ public class TryCatch {
 	Runnable
 	 */
 
-	static <E extends Throwable> void tryAndThrow(@NotNull ThrowingRunnable<E> tryMethod) {
+	public static <E extends Throwable> void tryAndThrow(@NotNull ThrowingRunnable<E> tryMethod) {
 		tryAndHandle(tryMethod, e -> {
 			if (e instanceof RuntimeException re) throw re;
 			throw new RuntimeException(e);
 		});
 	}
 
-	static <E extends Throwable> void tryAndIgnore(@NotNull ThrowingRunnable<E> tryMethod) {
+	public static <E extends Throwable> void tryAndIgnore(@NotNull ThrowingRunnable<E> tryMethod) {
 		tryAndHandle(tryMethod, e -> {});
 	}
 
-	static <E extends Throwable> void tryAndPrint(@NotNull ThrowingRunnable<E> tryMethod) {
+	public static <E extends Throwable> void tryAndPrint(@NotNull ThrowingRunnable<E> tryMethod) {
 		tryAndHandle(tryMethod, Throwable::printStackTrace);
 	}
 
-	static <E extends Throwable> void tryAndLog(@NotNull ThrowingRunnable<E> tryMethod, @NotNull Logger logger) {
+	public static <E extends Throwable> void tryAndLog(@NotNull ThrowingRunnable<E> tryMethod, @NotNull Logger logger) {
 		tryAndHandle(tryMethod, e -> logger.error("Failed to execute task", e));
 	}
 
 	@SuppressWarnings("unchecked")
-	static <E extends Throwable> void tryAndHandle(@NotNull ThrowingRunnable<E> tryMethod, @NotNull Consumer<E> catchMethod) {
+	public static <E extends Throwable> void tryAndHandle(@NotNull ThrowingRunnable<E> tryMethod, @NotNull Consumer<E> catchMethod) {
 		try {
 			tryMethod.run();
 		} catch(Throwable e) {
@@ -47,27 +47,27 @@ public class TryCatch {
 	Consumer
 	 */
 
-	static <E extends Throwable, T> void tryAndThrow(@NotNull ThrowingConsumer<E, T> tryMethod, T arg) {
+	public static <E extends Throwable, T> void tryAndThrow(@NotNull ThrowingConsumer<E, T> tryMethod, T arg) {
 		tryAndHandle(tryMethod, arg, e -> {
 			if (e instanceof RuntimeException re) throw re;
 			throw new RuntimeException(e);
 		});
 	}
 
-	static <E extends Throwable, T> void tryAndIgnore(@NotNull ThrowingConsumer<E, T> tryMethod, T arg) {
+	public static <E extends Throwable, T> void tryAndIgnore(@NotNull ThrowingConsumer<E, T> tryMethod, T arg) {
 		tryAndHandle(tryMethod, arg, e -> {});
 	}
 
-	static <E extends Throwable, T> void tryAndPrint(@NotNull ThrowingConsumer<E, T> tryMethod, T arg) {
+	public static <E extends Throwable, T> void tryAndPrint(@NotNull ThrowingConsumer<E, T> tryMethod, T arg) {
 		tryAndHandle(tryMethod, arg, Throwable::printStackTrace);
 	}
 
-	static <E extends Throwable, T> void tryAndLog(@NotNull ThrowingConsumer<E, T> tryMethod, T arg, @NotNull Logger logger) {
+	public static <E extends Throwable, T> void tryAndLog(@NotNull ThrowingConsumer<E, T> tryMethod, T arg, @NotNull Logger logger) {
 		tryAndHandle(tryMethod, arg, e -> logger.error("Failed to execute task", e));
 	}
 
 	@SuppressWarnings("unchecked")
-	static <E extends Throwable, T> void tryAndHandle(@NotNull ThrowingConsumer<E, T> tryMethod, T arg, @NotNull Consumer<E> catchMethod) {
+	public static <E extends Throwable, T> void tryAndHandle(@NotNull ThrowingConsumer<E, T> tryMethod, T arg, @NotNull Consumer<E> catchMethod) {
 		try {
 			tryMethod.accept(arg);
 		} catch(Throwable e) {
@@ -80,27 +80,27 @@ public class TryCatch {
 	Function
 	 */
 
-	static <E extends Throwable, T, R> Optional<R> tryAndThrow(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg) {
+	public static <E extends Throwable, T, R> Optional<R> tryAndThrow(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg) {
 		return tryAndHandle(tryMethod, arg, e -> {
 			if (e instanceof RuntimeException re) throw re;
 			throw new RuntimeException(e);
 		});
 	}
 
-	static <E extends Throwable, T, R> Optional<R> tryAndIgnore(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg) {
+	public static <E extends Throwable, T, R> Optional<R> tryAndIgnore(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg) {
 		return tryAndHandle(tryMethod, arg, e -> {});
 	}
 
-	static <E extends Throwable, T, R> Optional<R> tryAndPrint(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg) {
+	public static <E extends Throwable, T, R> Optional<R> tryAndPrint(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg) {
 		return tryAndHandle(tryMethod, arg, Throwable::printStackTrace);
 	}
 
-	static <E extends Throwable, T, R> Optional<R> tryAndLog(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg, @NotNull Logger logger) {
+	public static <E extends Throwable, T, R> Optional<R> tryAndLog(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg, @NotNull Logger logger) {
 		return tryAndHandle(tryMethod, arg, e -> logger.error("Failed to execute task", e));
 	}
 
 	@SuppressWarnings("unchecked")
-	static <E extends Throwable, T, R> Optional<R> tryAndHandle(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg, @NotNull Consumer<E> catchMethod) {
+	public static <E extends Throwable, T, R> Optional<R> tryAndHandle(@NotNull ThrowingFunction<E, T, R> tryMethod, T arg, @NotNull Consumer<E> catchMethod) {
 		try {
 			return Optional.ofNullable(tryMethod.apply(arg));
 		} catch(Throwable e) {
@@ -114,27 +114,27 @@ public class TryCatch {
 	Supplier
 	 */
 
-	static <E extends Throwable, T> Optional<T> tryAndThrow(@NotNull ThrowingSupplier<E, T> tryMethod) {
+	public static <E extends Throwable, T> Optional<T> tryAndThrow(@NotNull ThrowingSupplier<E, T> tryMethod) {
 		return tryAndHandle(tryMethod, e -> {
 			if (e instanceof RuntimeException re) throw re;
 			throw new RuntimeException(e);
 		});
 	}
 
-	static <E extends Throwable, T> Optional<T> tryAndIgnore(@NotNull ThrowingSupplier<E, T> tryMethod) {
+	public static <E extends Throwable, T> Optional<T> tryAndIgnore(@NotNull ThrowingSupplier<E, T> tryMethod) {
 		return tryAndHandle(tryMethod, e -> {});
 	}
 
-	static <E extends Throwable, T> Optional<T> tryAndPrint(@NotNull ThrowingSupplier<E, T> tryMethod) {
+	public static <E extends Throwable, T> Optional<T> tryAndPrint(@NotNull ThrowingSupplier<E, T> tryMethod) {
 		return tryAndHandle(tryMethod, Throwable::printStackTrace);
 	}
 
-	static <E extends Throwable, T> Optional<T> tryAndLog(@NotNull ThrowingSupplier<E, T> tryMethod, @NotNull Logger logger) {
+	public static <E extends Throwable, T> Optional<T> tryAndLog(@NotNull ThrowingSupplier<E, T> tryMethod, @NotNull Logger logger) {
 		return tryAndHandle(tryMethod, e -> logger.error("Failed to execute task", e));
 	}
 
 	@SuppressWarnings("unchecked")
-	static <E extends Throwable, T> Optional<T> tryAndHandle(@NotNull ThrowingSupplier<E, T> tryMethod, @NotNull Consumer<E> catchMethod) {
+	public static <E extends Throwable, T> Optional<T> tryAndHandle(@NotNull ThrowingSupplier<E, T> tryMethod, @NotNull Consumer<E> catchMethod) {
 		try {
 			return Optional.ofNullable(tryMethod.get());
 		} catch(Throwable e) {
